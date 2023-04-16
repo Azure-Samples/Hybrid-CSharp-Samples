d79f4fdd4060a5d32ebab053cbdf5165a0c2ba35
Sarathy Sakshi <username@users.noreply.github.com>
2023-04-16 10:57:15 -0700

    update dotnet sample

---
+++ resourcegroup/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Azure.Samples
+{
+    internal static class AzureStackConstants
+    {
+        public const string StsUri = "https://login.azs:3001";
+        public const string ArmUri = "https://resourcemanagerweb.azs:40007";
+        public const string Audience = "https://management.azs";
+        public const string TenantId = "98b8267d-e97f-426e-8b3f-7956511fd63f";
+        public const string SubscriptionId = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee";
+        public const string ClientId = "2f3a3aa4-12ba-4e04-93f5-005d681a315c";
+    }
+}
--- resourcegroup/Program.cs
+++ resourcegroup/Program.cs
@@ -1,116 +1,86 @@
-﻿using Azure.Identity;
-using Azure.ResourceManager;
-using Azure.ResourceManager.Resources;
-using System;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
-using System.IO;
-using System.Net.Http;
-using System.Collections.Generic;
-
-namespace ResourceGroup
+﻿namespace Azure.Samples
 {
+    using Azure;
+    using Azure.Core;
+    using Azure.Identity;
+    using Azure.ResourceManager;
+    using Azure.ResourceManager.Resources;
+    using System;
+    using System.Threading.Tasks;
+    using Xunit;
+
     public class Program
     {
-        // AuthorityHost is the login endpoint or the active directory authority.
-        public static Uri AuthorityHost;
-        public static string Audiences;
-        static readonly HttpClient httpClient = new HttpClient();
-        public static async Task runSample(string tenantId, string subscriptionId, string servicePrincipalId, string servicePrincipalSecret, string location, string armEndpoint)
+        readonly DefaultAzureCredential Credentials;
+        Uri ArmUri = new Uri(AzureStackConstants.ArmUri);
+        ArmClient TestArmClient;
+
+        public Program()
         {
-            await SetEnvironmentEndpoints(armEndpoint);
-            Console.WriteLine("Creating ClientSecretCredential...");
-            string[] urlSegments = AuthorityHost.AbsoluteUri.Split('/');
-            if (urlSegments[urlSegments.Length - 1] == "adfs")
+            Environment.SetEnvironmentVariable("AZURE_SUBSCRIPTION_ID", AzureStackConstants.SubscriptionId);
+            Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", AzureStackConstants.ClientId); 
+            Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "sample");
+            Environment.SetEnvironmentVariable("AZURE_TENANT_ID", AzureStackConstants.TenantId);
+
+            var credOptions = new DefaultAzureCredentialOptions
             {
-                tenantId = "adfs";
-            }
-            var credential = new ClientSecretCredential(tenantId, servicePrincipalId, servicePrincipalSecret, new TokenCredentialOptions {AuthorityHost = AuthorityHost});
-            Console.WriteLine("Creating ArmClient...");
-            var armClientOptions = new ArmClientOptions {
-                Environment = new ArmEnvironment(new Uri(armEndpoint), Audiences)
+                AuthorityHost = new Uri(AzureStackConstants.StsUri),
+                TenantId = AzureStackConstants.TenantId,
+                DisableInstanceDiscovery = true
             };
-            armClientOptions.SetApiVersionsFromProfile(AzureStackProfile.Profile20200901Hybrid);
-            var armClient = new ArmClient(credential, subscriptionId, armClientOptions);
-            SubscriptionResource subscription = armClient.GetDefaultSubscription();
-            var resourceGroupName = "azure-sample-csharp-resourcegroup";
-            ResourceGroupData resourceGroupData = new ResourceGroupData(location);
-            ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
-            ResourceGroupResource resourceGroup;
+            Credentials = new DefaultAzureCredential(credOptions);
 
-            try
+            var options = new ArmClientOptions
             {
-                // Create resource group.
-                Console.WriteLine(String.Format("Creating a resource group with name: {0}", resourceGroupName));
-                ArmOperation<ResourceGroupResource> operation = await resourceGroups.CreateOrUpdateAsync(Azure.WaitUntil.Completed, resourceGroupName, resourceGroupData);
-                resourceGroup = operation.Value;
+                Environment = new ArmEnvironment(ArmUri, AzureStackConstants.Audience),
+            };
 
-                // Add tag to resource group.
-                var tagName = "tagName";
-                var tagValue = "tagValue";
-                resourceGroup.AddTag(tagName, tagValue);
-                var tags = new Dictionary<string, string>()
+            options.SetApiVersionsFromProfile(AzureStackProfile.Profile20200901Hybrid);
+            TestArmClient = new ArmClient(Credentials, AzureStackConstants.SubscriptionId, options);
+        }
+
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.CreateResourceGroup();
+            program.GetResourceGroups();
+        }
+
+        [Fact]
+        public void GetResourceGroups()
+        {
+            var subscription = TestArmClient.GetDefaultSubscription();
+            Console.WriteLine($"Default Azure Subscription: {subscription.Id}");
+            var resourceGroups = subscription.GetResourceGroups();
+
+            foreach (var rg in resourceGroups)
+            {
+                Console.WriteLine(rg.Id);
+                foreach (var resource in rg.GetGenericResources())
                 {
-                    {"a", "b"},
-                    {"c", "d"}
-                };
-                resourceGroup.SetTags(tags);
-                resourceGroup.RemoveTag("a");
+                    Console.WriteLine($"\t{resource.Id}");
+                }
+            }
 
-                // Can also update tags via the following code:
-                // ResourceGroupPatch resourceGroupPatch = new ResourceGroupPatch();
-                // resourceGroupPatch.Tags[tagName] = tagValue;
-                // Console.WriteLine(String.Format("Adding tags '{0}:{1}' to resource group {2}", tagName, tagValue, resourceGroupName));
-                // resourceGroup.Update(resourceGroupPatch);
-
-                // Get resource group.
-                Console.WriteLine(String.Format("Getting resource group {0}", resourceGroupName));
-                ResourceGroupResource gotResourceGroup = subscription.GetResourceGroup(resourceGroupName);
-                Console.WriteLine(String.Format("Got resource group: {0}", gotResourceGroup));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(String.Format("Could not create resource group {0}. Exception: {1}", resourceGroupName, ex.Message));
-            }
-            finally
-            {
-                // Delete resource group.
-                resourceGroup = subscription.GetResourceGroup(resourceGroupName);
-                Console.WriteLine(String.Format("Deleting a resource group with name: {0}", resourceGroupName));
-                await resourceGroup.DeleteAsync(Azure.WaitUntil.Completed);
-            }
+            Assert.True(subscription != null, "Expect default subscription to be not null.");
         }
 
-        static async Task SetEnvironmentEndpoints(String armEndpoint)
+        [Fact]
+        public async Task CreateResourceGroup()
         {
-            try
-            {
-                string responseBody = await httpClient.GetStringAsync(string.Format("{0}/metadata/endpoints?api-version=2019-10-01", armEndpoint));
-                var deserializedArray = JArray.Parse(responseBody);
-                var deserializedObject = deserializedArray[0].Value<JObject>();
-                var authenticationObj = deserializedObject.GetValue("authentication").Value<JObject>();
-                var loginEndpoint = authenticationObj.GetValue("loginEndpoint").Value<string>();
-                var audiencesObj = authenticationObj.GetValue("audiences").Value<JArray>();
-                AuthorityHost = new Uri(loginEndpoint);
-                Audiences = audiencesObj[0].Value<string>();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(String.Format("Could not get Azure Stack environment service metadata. Exception: {0}", ex.Message));
-            }
-        }
+            var subscription = TestArmClient.GetDefaultSubscription();
+            var resourceGroups = subscription.GetResourceGroups();
+            AzureLocation location = new AzureLocation("DevFabric");
 
-        public static async Task Main(string[] args)
-        {
-            JObject secretServicePrincipalSettings = JObject.Parse(File.ReadAllText(@"..\azureSecretSpConfig.json"));
-            var tenantId = secretServicePrincipalSettings.GetValue("tenantId").ToString();
-            var servicePrincipalId = secretServicePrincipalSettings.GetValue("clientId").ToString();
-            var servicePrincipalSecret = secretServicePrincipalSettings.GetValue("clientSecret").ToString();
-            var subscriptionId = secretServicePrincipalSettings.GetValue("subscriptionId").ToString();
-            var resourceManagerEndpointUrl = secretServicePrincipalSettings.GetValue("resourceManagerEndpointUrl").ToString();
-            var location = secretServicePrincipalSettings.GetValue("location").ToString();
+            string resourceGroupName = "myRgName" + Guid.NewGuid().ToString();
+            ResourceGroupData resourceGroupData = new ResourceGroupData(location);
+            ArmOperation<ResourceGroupResource> operation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
+            ResourceGroupResource resourceGroup = operation.Value;
+            await Console.Out.WriteLineAsync(resourceGroup.Id);
 
-            await runSample(tenantId, subscriptionId, servicePrincipalId, servicePrincipalSecret, location, resourceManagerEndpointUrl);
+            var rgResource = resourceGroups.Get(resourceGroupName);
+            await Console.Out.WriteLineAsync(rgResource.Value.ToString());
+            Assert.True(rgResource != null, "Expect created resource group is nont null.");
         }
     }
 }
